66a10b7a5f0b62d98d9db226fe709bf8f2927d36
unknown <a.n.royo@outlook.com>
2025-01-21 23:16:58 +0800

    add test controller

---
+++ src/main/java/com/springapi/springapi/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.springapi.springapi.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+@CrossOrigin(origins = "*")
+public class TestController {
+
+
+
+    @RequestMapping(value = "/getRecord", method = RequestMethod.GET)
+    public String displayString(){
+        return "Iloveyou love";
+    }
+}
